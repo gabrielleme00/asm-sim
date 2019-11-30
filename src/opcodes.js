c86bfd58ece2faaf8827164ad961605c8d4dc625
Gabriel Leme <bielleme2012@hotmail.com>
2019-11-30 14:27:08 -0300

    Added all opcodes; some info is still missing

--- src/opcodes.js
+++ src/opcodes.js
@@ -22,11 +22,11 @@ export const Opcodes = {
     MOV_NUMBER_TO_REGADDRESS: 8,    // Needs next 2 bytes
 
     // ADD
-    ADD_REG_TO_REG: 9,              // Needs next 1 byte
-    ADD_REGADDRESS_TO_REG: 10,      // Needs next 1 byte
-    ADD_ADDRESS_TO_REG: 11,         // Needs next 2 bytes
-    ADD_NUMBER_TO_REG: 12,          // Needs next 2 bytes
-    
+    ADD_REG_TO_REG: 10,             // Needs next 1 byte
+    ADD_REGADDRESS_TO_REG: 11,      // Needs next 1 byte
+    ADD_ADDRESS_TO_REG: 12,         // Needs next 2 bytes
+    ADD_NUMBER_TO_REG: 13,          // Needs next 2 bytes
+
     // SUB
     SUB_REG_FROM_REG: 14,           // Needs next 1 byte
     SUB_REGADDRESS_FROM_REG: 15,    // Needs next 1 byte
@@ -36,34 +36,89 @@ export const Opcodes = {
     // INC, DEC
     INC_REG: 18,                    // Needs next 1 byte
     DEC_REG: 19,                    // Needs next 1 byte
-    
+
     // CMP
     CMP_REG_WITH_REG: 20,           // Needs next 1 byte
     CMP_REGADDRESS_WITH_REG: 21,    // Needs next 1 byte
     CMP_ADDRESS_WITH_REG: 22,       // Needs next 2 bytes
     CMP_NUMBER_WITH_REG: 23,        // Needs next 2 bytes
-    
-    // JMP variants
-    JMP_REGADDRESS: 24,             // Needs next 1 byte
-    JMP_ADDRESS: 25,                // Needs next 1 byte
-    JC_REGADDRESS: 26,              // Needs next 1 byte
-    JC_ADDRESS: 27,                 // Needs next 1 byte
-    JNC_REGADDRESS: 28,
-    JNC_ADDRESS: 29,
-    JZ_REGADDRESS: 30,
-    JZ_ADDRESS: 31,
-    JNZ_REGADDRESS: 32,
-    JNZ_ADDRESS: 33,
-    JA_REGADDRESS: 34,              // Needs next 1 byte
-    JA_ADDRESS: 35,                 // Needs next 1 byte
-    JNA_REGADDRESS: 36,
-    JNA_ADDRESS: 37,
-    // http://marco.uminho.pt/~joao/Computacao2/node47.html
 
-    // PUSH, POP
-    // CALL, RET
-    // MUL, DIV
-    // AND, OR, XOR, NOT, SHL, SHR
+    // JMP
+    JMP_REGADDRESS: 30,             // Needs next 1 byte
+    JMP_ADDRESS: 31,                // Needs next 1 byte
+    JC_REGADDRESS: 32,              // Needs next 1 byte
+    JC_ADDRESS: 33,                 // Needs next 1 byte
+    JNC_REGADDRESS: 34,             // Needs next 1 byte
+    JNC_ADDRESS: 35,                // Needs next 1 byte
+    JZ_REGADDRESS: 36,              // Needs next 1 byte
+    JZ_ADDRESS: 37,                 // Needs next 1 byte
+    JNZ_REGADDRESS: 38,             // Needs next 1 byte
+    JNZ_ADDRESS: 39,                // Needs next 1 byte
+    JA_REGADDRESS: 40,              // Needs next 1 byte
+    JA_ADDRESS: 41,                 // Needs next 1 byte
+    JNA_REGADDRESS: 42,             // Needs next 1 byte
+    JNA_ADDRESS: 43,                // Needs next 1 byte
+
+    // PUSH
+    PUSH_REG: 50,
+    PUSH_REGADDRESS: 51,
+    PUSH_ADDRESS: 52,
+    PUSH_NUMBER: 53,
+
+    // POP
+    POP_REG: 54,
+
+    // CALL
+    CALL_REGADDRESS: 55,
+    CALL_ADDRESS: 56,
+
+    // RET
+    RET: 57,
+
+    // MUL
+    MUL_REG: 60,
+    MUL_REGADDRESS: 61,
+    MUL_ADDRESS: 62,
+    MUL_NUMBER: 63,
+
+    // DIV
+    DIV_REG: 64,
+    DIV_REGADDRESS: 65,
+    DIV_ADDRESS: 66,
+    DIV_NUMBER: 67,
+
+    // AND
+    AND_REG_WITH_REG: 70,
+    AND_REGADDRESS_WITH_REG: 71,
+    AND_ADDRESS_WITH_REG: 72,
+    AND_NUMBER_WITH_REG: 73,
+
+    // OR
+    OR_REG_WITH_REG: 74,
+    OR_REGADDRESS_WITH_REG: 75,
+    OR_ADDRESS_WITH_REG: 76,
+    OR_NUMBER_WITH_REG: 77,
+
+    // XOR
+    XOR_REG_WITH_REG: 78,
+    XOR_REGADDRESS_WITH_REG: 79,
+    XOR_ADDRESS_WITH_REG: 80,
+    XOR_NUMBER_WITH_REG: 81,
+
+    // NOT
+    NOT_REG: 82,
+
+    // SHL
+    SHL_REG_WITH_REG: 90,
+    SHL_REGADDRESS_WITH_REG: 91,
+    SHL_ADDRESS_WITH_REG: 92,
+    SHL_NUMBER_WITH_REG: 93,
+
+    // SHR
+    SHR_REG_WITH_REG: 94,
+    SHR_REGADDRESS_WITH_REG: 95,
+    SHR_ADDRESS_WITH_REG: 96,
+    SHR_NUMBER_WITH_REG: 97,
     
     STOP: 255
 }
