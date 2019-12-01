4f1a95f829c9c28ed0a02aded588ae76bd45a513
Gabriel Leme <bielleme2012@hotmail.com>
2019-12-01 12:39:58 -0300

    Updated compiler test code

--- src/main.js
+++ src/main.js
@@ -14,17 +14,16 @@ const code = `
     INC B           ; Increases B register
 
     ADD A, B        ; Add reg to reg
-    ADD A, [B]      ; Add regaddress to reg
-    ADD A, [0x0]    ; Add address to reg
-    ADD A, [B+0x0C] ; Add address to reg
-    ADD A, 2        ; Add number to reg
-    ADD A, 0x03     ; Add number to reg
 
     HLT         ; Stops the computer
 `;
-const compiled = compiler.compile(code);
 
-computer.memory.set(0, compiled);
-computer.start(1);
+try {
+    const compiled = compiler.compile(code);
 
-console.log(computer);
+    computer.memory.set(0, compiled);
+    computer.start(1);
+    
+} catch (error) {
+    console.error(error);
+}
