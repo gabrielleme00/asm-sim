bfc85a3be8810534d3714c5188d4b8a258d20ea6
Gabriel Leme <bielleme2012@hotmail.com>
2019-12-01 13:20:47 -0300

    ADD can now be compiled; helper functions added

--- src/compiler.js
+++ src/compiler.js
@@ -8,6 +8,8 @@ import { Opcodes } from './opcodes.js';
  * @param {String} char GP Register (A, B, C, D)
  */
 function getReg(name) {
+    name = name.replace(/\[|\]/g, '');
+
     switch (name.toUpperCase()) {
         case 'A':
             return 0;
@@ -26,11 +28,82 @@ function getReg(name) {
     }
 }
 
+function hasEnoughArgs(instr, args) {
+    switch (instr) {
+        case "INC":
+            return args.length >= 1;
+
+        case "ADD":
+        case "DEC":
+            return args.length >= 2;
+
+        default:
+            throw "Unknown arguments count for " + instr;
+    }
+}
+
+/**
+ * Returns true if the given string can be
+ * converted to an integer.
+ * @param {String} str Any string
+ */
+function isNum(str) {
+    return Number.isInteger(parseInt(str.trim()));
+}
+
+/**
+ * Returns true if the given string represents
+ * a valid register.
+ * @param {String} str Any string
+ */
+function isReg(str) {
+    switch (str.trim().toUpperCase()) {
+        case 'A':
+        case 'B':
+        case 'C':
+        case 'D':
+            return true;
+
+        default:
+            return false;
+    }
+}
+
+/**
+ * Returns true if the given string represents
+ * an address.
+ * @param {String} str Any string
+ */
+function isAddr(str) {
+    // Check if the string has at least 3 chars ("[X]")
+    if (str.length < 3) {
+        return false;
+    }
+
+    // Check if the first and the last chars indicate an address
+    if (str[0] != '[' || str[str.length - 1] != ']') {
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * Returns true if the given string represents
+ * the address of a register.
+ * @param {String} str Any string
+ */
+function isRegAddr(str) {
+    // Check if the string has 3 chars ("[X]")
+    // and if the inner char is a valid register
+    return str.length === 3 && isReg(str.charAt(1));
+}
+
 export class Compiler {
     /**
      * Compiles a given raw assembly code
      * and returns an array of bytes.
-     * @param {String} rawCode Assembly code
+     * @param {String} rawCode Raw assembly code
      */
     compile(rawCode) {
         let result = [];
@@ -41,19 +114,28 @@ export class Compiler {
             .split('\n')
             .map(line => line.trim().replace(/ +/g, ' '));
 
-        lines.forEach(line => {
+        lines.forEach((line, i) => {
+            // Skip empty lines
             if (line == "") return;
 
+            // Gets the exact uppercase instruction
             let instr = line.slice(0, 3).toUpperCase();
+
+            // Array of arguments
             let args = line
                 .slice(4, line.length)
                 .replace(/ +/g, '')
                 .split(',');
 
-            this.interpret(instr, args).forEach(value => {
-                result.push(value);
-            });
-            
+            try {
+                // Interpret each line
+                this.interpret(instr, args).forEach(value => {
+                    result.push(value);
+                });
+            } catch (error) {
+                throw `Compile error at line ${i}:\n${error}`;
+            }
+
         });
 
         return result;
@@ -68,34 +150,63 @@ export class Compiler {
      * @param {Array<String>} args Array of arguments
      */
     interpret(instr, args) {
-        const opcodes = Opcodes;
+        const op = Opcodes;
         let result = [];
+
+        // Check HALT instruction
+        if (instr == "HLT") {
+            result.push(0);
+            return result;
+        }
+
+        // Check if argument count is enough for the instruction
+        if (!hasEnoughArgs(instr, args)) {
+            throw instr + " needs more arguments";
+        }
 
         switch (instr) {
             case "INC":
-                result.push(opcodes.INC_REG);
+                result.push(op.INC_REG);
                 result.push(getReg(args[0]));
                 break;
 
             case "DEC":
-                result.push(opcodes.DEC_REG);
+                result.push(op.DEC_REG);
                 result.push(getReg(args[0]));
                 break;
 
             case "ADD":
-                // TODO
-                // Check if arg 2 exists
-                // Check type of arg 2
-                // Push correct values to result
-                
-                break;
+                if (isNum(args[1])) {
+                    result.push(op.ADD_NUMBER_TO_REG);
+                    result.push(getReg(args[0]));
+                    result.push(utils.confine(parseInt(args[1])));
 
-            case "HLT":
-                result.push(0);
+                } else if (isReg(args[1])) {
+                    result.push(op.ADD_REG_TO_REG);
+                    result.push(utils.compress(
+                        getReg(args[0]), getReg(args[1])
+                    ));
+
+                } else if (isRegAddr(args[1])) {
+                    result.push(op.ADD_REGADDRESS_TO_REG);
+                    result.push(utils.compress(
+                        getReg(args[0]), getReg(args[1]) 
+                    ));
+
+                } else if (isAddr(args[1])) {
+                    // TODO: Support relative-to-reg-value addresses
+                    result.push(op.ADD_ADDRESS_TO_REG);
+                    result.push(getReg(args[0]));
+                    result.push(utils.confine(parseInt(args[1])));
+                } else {
+                    throw "2nd argument is of unknown type in "
+                    + instr + " instruction";
+
+                }
                 break;
 
             default:
-                throw "Command unknown: " + instr;
+                throw "Instruction unknown: " + instr;
         }
 
         return result;
